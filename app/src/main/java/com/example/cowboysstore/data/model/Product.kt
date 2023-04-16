50cef2d7271971118313c6c725108ef27da606ea
AntonLm423 <anton.kiselyov02@mail.ru>
2023-04-16 16:49:47 +0300

    Initial commit

--- app/src/main/java/com/example/cowboysstore/data/model/Product.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cowboysstore.data.model
-
-data class Product(
-    val id : String,
-    val title : String,
-    val category : String,
-    val price : String,
-    val preview : String
-)
--- app/src/main/java/com/example/cowboysstore/presentation/adapters/MenuItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cowboysstore.presentation.adapters
-
-data class MenuItem(
-    val id : Int,
-    val title: String,
-    val iconResId: Int
-    )
